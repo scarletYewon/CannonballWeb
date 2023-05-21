86b6692959c7aa70f8989b2d5f44efe06dbfba99
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-05-22 01:10:10 +0900

    main demo

--- webfront/src/index.js
+++ webfront/src/index.js
@@ -7,8 +7,39 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    캐논볼 웹사이트 만들기
+    <div>
+    {/* 캐논볼 로고 들어가는자리 */}
+      <div style={{
+        textAlign: 'center',
+        marginTop: '20px',
+      }}>
+        <img className='logoImage' src='img/header.png' />
+      </div>
+    </div>
+    <div>
+      {/* 참가신청하는자리 */}
+      <div style={{
+        textAlign: 'center'
+      }}>
+        <img className='body' src='img/body.png' />
+      </div>
+    </div>
+    <div>
+      {/* 환영합니다 */}
+      <div style={{
+        marginTop: '50px'
+      }}>
+        <img className='welcome' src='img/welcome.svg' />
+      </div>
+    </div>
+    <div>
+      {/* 동아리 소개페이지 */}
+      <div>
+        <img className='introduce' src='img/introduce.svg' />
+      </div>
+    </div>
   </div>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
