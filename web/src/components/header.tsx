5d89e188c3c0f4ee9abefafddb04488b41194254
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-08-21 23:50:26 +0900

    serverTest 1차
    API 16개중 14개 성공 1개 미구현 1개 실패

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -4,87 +4,59 @@ import { useNavigate } from 'react-router-dom';
 function Header() {
   const navigate = useNavigate();
 
-  const goSignin = () => {
-    navigate('/signin');
-  }
-  const goSignup = () => {
-    navigate('/signup');
-  }
-  const goMain = () => {
-    navigate('/');
-  }
+  // const goSignin = () => {
+  //   navigate('/signin');
+  // }
+  // const goSignup = () => {
+  //   navigate('/signup');
+  // }
+  // const goMain = () => {
+  //   navigate('/');
+  // }
 
   return (
     <div
-        id='header'
-        style={{
-          display: 'inline',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
+      style={{
+        display: 'flex',
+      }}
     >
-      <img
-        id='logo'
-        style={{
-          marginTop: '.8rem',
-          marginBottom: '.8rem',
-          width: '15rem',
-          height: '3rem',
-          marginLeft: '8rem'
-        }}
-        src='img/logorow.svg'
-        onClick={goMain}
-        />
+      <img src="img/logo_white.png" />
       <div
-        id='itemBox'
         style={{
-          marginTop: '.8rem',
-          marginBottom: '.8rem',
-          height: '3rem',
           display: 'flex',
-          marginRight: '8rem',
-          justifyContent: 'center',
-          alignItems: 'center',
-          float: 'right',
         }}
       >
-        <div
-          id='signIn'
-          style={{
-            width: '5rem',
-            display: 'block',
-            textAlign: 'center',
-            margin: '.25rem',
-            padding: '.5rem 1rem',
-            color: 'black',
-            border: '1px solid #000000',
-            borderRadius: '4px',
-            background: 'white',
-          }}
-          onClick={goSignin}
-        >
-          Sign In
+        <div>
+          <img src="img/introduction.png"/>
         </div>
-        <div
-          id='signUP'
-          style={{
-            width: '5rem',
-            display: 'block',
-            textAlign: 'center',
-            margin: '.25rem',
-            padding: '.5rem 1rem',
-            color: 'white',
-            border: '1px solid #000000',
-            borderRadius: '4px',
-            background: 'black',
-          }}
-          onClick={goSignup}
-        >
-          Sign Up
+        <div>
+          <img src="img/vision.png"/>
+        </div>
+        <div>
+          <img src="img/ouractivity.png"/>
+        </div>
+        <div>
+          <img src="img/more.png"/>
         </div>
       </div>
+      <div
+        style={{
+          display: 'flex',
+        }}
+      >
+        <div>
+          <img src="img/login.png"/>
+        </div>
+        <div>
+          <img src="img/signup.png"/>
+        </div>
+        <div>
+          <img src="img/manager.png"/>
+        </div>
+      </div>
+      
+  
     </div>
-
     
   );
 }
