446a8ddeefacce843e44ea3b75fbe5bfac73a37b
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-09-15 17:48:55 +0900

    eventManagement

--- web/src/pages/managePages/event.tsx
+++ web/src/pages/managePages/event.tsx
@@ -6,6 +6,60 @@ function Event(){
   return(
     <div>
       <Header/>
+      <div
+        style={{
+          height: '88vh',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            margin: '0 auto',
+            alignItems: 'center',
+          }}
+        >
+          <div
+            style={{
+              textAlign: 'center',
+              marginBottom: '1em',
+            }}
+          >
+            <img src="img/upicon.svg"/>
+          </div>
+          <div>
+            <img
+              src="img/addaevent.svg"
+              style={{
+                marginRight: '0.5em',
+              }}
+            />
+            <img
+              src="img/management.svg"
+              style={{
+                marginLeft: '0.5em',
+              }}
+            />
+          </div>
+          <div
+            style={{
+              textAlign: 'center',
+              marginTop: '1em'
+            }}
+          >
+            <img src="img/downicon.svg"/>
+          </div>
+          <div
+            style={{
+              textAlign: 'center',
+              marginTop: '1em'
+            }}
+          >
+            <img src="img/clickthebutton.svg"/>
+          </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
