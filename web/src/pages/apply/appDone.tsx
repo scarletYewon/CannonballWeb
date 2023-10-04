017833526b39f83a68c2116ef5a11d4ab657c390
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-10-04 22:52:31 +0900

    appDone

--- web/src/pages/apply/appDone.tsx
+++ web/src/pages/apply/appDone.tsx
@@ -6,8 +6,69 @@ function AppDone(){
   return(
     <div>
       <Header/>
-      <div>
-        
+      <div
+        style={{
+          height: '88vh',
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div
+          style={{
+            margin: '0 auto',
+            alignItems: 'center',
+          }}
+        >
+          <div
+            style={{
+              textAlign: 'center',
+            }}
+          >
+            <div
+              style={{
+                fontSize: '4em',
+                fontWeight:'800',
+              }}
+            >
+            신청이
+            </div>
+            <div
+              style={{
+                fontSize: '4em',
+                fontWeight:'800',
+              }}
+            >
+              완료되었습니다
+            </div>
+          </div>
+          
+          <div
+            style={{
+              textAlign: 'center',
+              fontSize: '1em',
+              fontWeight: '400',
+              marginTop: '3em',
+            }}
+          >
+            조 확인을 원하시면 밑의 버튼을 눌러주세요
+          </div>
+          <div
+            style={{
+              textAlign: 'center',
+              marginTop: '1.5em',
+            }}
+          >
+            <img src="img/line_appdone.svg"/> 
+          </div>
+          <div
+            style={{
+              textAlign: 'center',
+            }}
+          >
+            <img src="img/btn_appdone.svg"/> 
+          </div>
+          
+        </div>
       </div>
     </div>
   )
