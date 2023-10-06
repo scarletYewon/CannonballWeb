b387b8787945b63eba571d9689e67746da815bc8
이예원 <81704418+scarletYewon@users.noreply.github.com>
2023-10-06 19:50:13 +0900

    teams

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,6 +14,7 @@ import Application from './pages/apply/application';
 import AppDone from './pages/apply/appDone';
 import TeamMember from './pages/apply/teamMember';
 import AddEvent from './pages/managePages/addEvent';
+import Teams from './pages/apply/teams';
 
 const App:React.FC = (): JSX.Element => {
   const onSubmit = (form: { name: string, description: string}) => {
@@ -21,6 +22,7 @@ const App:React.FC = (): JSX.Element => {
   }
   return (
     <Routes>
+      {/* main 페이지 */}
       <Route path='/' element={<Main/>}/>
       <Route path='/signin' element={<Signin onSubmit={onSubmit}/>}/>
       <Route path='/signup' element={<Signup/>}/>
@@ -28,13 +30,18 @@ const App:React.FC = (): JSX.Element => {
       <Route path='/vision' element={<Vision/>}/>
       <Route path='/ouractivity' element={<OurActivity/>}/>
       <Route path='/more' element={<More/>}/>
+      
+      {/* 메니저페이지 */}
       <Route path='/manager' element={<Manager/>}/>
       <Route path='/managermember' element={<Member/>}/>
       <Route path='/managerevent' element={<Event/>}/>
+      <Route path='/addevent' element={<AddEvent/>}/>
+
+      {/* apply 페이지 */}
       <Route path='/application' element={<Application/>}/>
       <Route path='/appdone' element={<AppDone/>}/>
       <Route path='/teammember' element={<TeamMember/>}/>
-      <Route path='/addevent' element={<AddEvent/>}/>
+      <Route path='/teams' element={<Teams/>}/>
     </Routes>
   );
 }
--- web/src/pages/apply/teams.tsx
+++ web/src/pages/apply/teams.tsx
@@ -1,6 +1,193 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Header from "../../components/header";
 
-function teams(){
-  
+function Teams(){
+
+  return(
+    <div
+      style={{
+        margin: '0',
+        background: '#D3D3D3',
+      }}
+    >
+      <Header/>
+      <div
+        style={{
+          textAlign: 'center',
+          marginTop: '4em',
+          paddingBottom: '5em',
+        }}
+      >
+        <div>
+          <img src="img/icon_team.svg"/>
+        </div>
+        <div>
+          <img src="img/line_appdone.svg"/>
+        </div>
+        <div
+          style={{
+            // background: 'white',
+            display: 'inline-block',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+            }}
+          >
+            <div
+              style={{
+                margin: '2em',
+              }}
+            >
+              <img 
+                style={{
+                  width: '15em',
+                }}
+              src="img/img_team.svg"/>
+              <div
+                style={{
+                  padding:'2em',
+                  fontSize: '1.5em',
+                  fontWeight: '800',
+                  color:'#636161',
+                  background: 'white',
+                }}
+              >
+                Team1
+              </div>
+            </div>
+            <div
+              style={{
+                margin: '2em',
+              }}
+            >
+              <img 
+                style={{
+                  width: '15em',
+                }}
+              src="img/img_team.svg"/>
+              <div
+                style={{
+                  padding:'2em',
+                  fontSize: '1.5em',
+                  fontWeight: '800',
+                  color:'#636161',
+                  background: 'white',
+                }}
+              >
+                Team2
+              </div>
+            </div>
+            <div
+              style={{
+                margin: '2em',
+              }}
+            >
+              <img 
+                style={{
+                  width: '15em',
+                }}
+              src="img/img_team.svg"/>
+              <div
+                style={{
+                  padding:'2em',
+                  fontSize: '1.5em',
+                  fontWeight: '800',
+                  color:'#636161',
+                  background: 'white',
+                }}
+              >
+                Team3
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* 한줄더 */}
+        <div
+          style={{
+            // background: 'white',
+            display: 'inline-block',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+            }}
+          >
+            <div
+              style={{
+                margin: '2em',
+              }}
+            >
+              <img 
+                style={{
+                  width: '15em',
+                }}
+              src="img/img_team.svg"/>
+              <div
+                style={{
+                  padding:'2em',
+                  fontSize: '1.5em',
+                  fontWeight: '800',
+                  color:'#636161',
+                  background: 'white',
+                }}
+              >
+                Team4
+              </div>
+            </div>
+            <div
+              style={{
+                margin: '2em',
+              }}
+            >
+              <img 
+                style={{
+                  width: '15em',
+                }}
+              src="img/img_team.svg"/>
+              <div
+                style={{
+                  padding:'2em',
+                  fontSize: '1.5em',
+                  fontWeight: '800',
+                  color:'#636161',
+                  background: 'white',
+                }}
+              >
+                Team5
+              </div>
+            </div>
+            <div
+              style={{
+                margin: '2em',
+              }}
+            >
+              <img 
+                style={{
+                  width: '15em',
+                }}
+              src="img/img_team.svg"/>
+              <div
+                style={{
+                  padding:'2em',
+                  fontSize: '1.5em',
+                  fontWeight: '800',
+                  color:'#636161',
+                  background: 'white',
+                }}
+              >
+                Team6
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+
 }
+export default Teams;
